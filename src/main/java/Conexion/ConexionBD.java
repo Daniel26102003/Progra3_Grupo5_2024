83ae6c5813a337264873b8d403b8215bb449f742
Esaú <Esa@DESKTOP-DID1KRB>
2024-03-14 12:19:45 -0600

    Push de conexion a BD y frame Cargar_ArchivoBD

---
+++ src/main/java/Conexion/ConexionBD.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Esaú
+ */
+
+public class ConexionBD {
+    Connection conn;
+    private String host = "localhost";
+    private String port = "3306";
+    private String dbName = "proyectoGrupo5";
+    private String userName = "root";
+    private String password = "Grupo5";
+    
+    public ConexionBD (){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + this.host + ":" + this.port + "/" + this.dbName;
+            conn = DriverManager.getConnection(url, this.userName, this.password);
+            System.out.println("Conexion exitosa");
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("Error no se conecto");   
+        }
+            
+    }
+    
+}
+
